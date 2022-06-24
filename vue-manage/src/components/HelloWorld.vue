a3e3f19aed4cee8f3fd059af758a3c83d0831288
zhixiang-pan <zhixiang_pan@trendmicro.com>
2022-06-24 17:40:39 +0800

    使用VueRouter配置路由

--- vue-manage/src/components/HelloWorld.vue
+++ vue-manage/src/components/HelloWorld.vue
@@ -1,12 +1,12 @@
 <template>
     <div class="hello">
         <el-row>
-            <el-button>默认按钮</el-button>
-            <el-button type="primary">主要按钮</el-button>
-            <el-button type="success">成功按钮</el-button>
-            <el-button type="info">信息按钮</el-button>
-            <el-button type="warning">警告按钮</el-button>
-            <el-button type="danger">危险按钮</el-button>
+            <router-link to="/">
+                <el-button>默认按钮</el-button>
+            </router-link>
+            <router-link to="/user">
+                <el-button type="primary">主要按钮</el-button>
+            </router-link>
         </el-row>
     </div>
 </template>
