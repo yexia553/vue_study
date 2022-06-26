4e2650e293e29437be2eb1cbef404ee035cae751
zhixiang-pan <zhixiang_pan@trendmicro.com>
2022-06-26 22:45:36 +0800

    初始化左侧侧边栏

--- vue-manage/src/App.vue
+++ vue-manage/src/App.vue
@@ -11,7 +11,7 @@ export default {
 }
 </script>
 
-<style>
+<!-- <style>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -20,4 +20,4 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
 }
-</style>
+</style> -->
