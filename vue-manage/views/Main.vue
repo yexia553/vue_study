a4d05b4c2f9078616f4127aa9e351e9655fb580e
zhixiang-pan <zhixiang_pan@trendmicro.com>
2022-06-27 15:51:04 +0800

    添加注释

--- vue-manage/views/Main.vue
+++ vue-manage/views/Main.vue
@@ -1,11 +1,13 @@
 <template>
     <el-container style="height: 100%">
         <el-aside width="auto">
+            <!-- 侧边栏可以看做一个公共模块，不论在什么页面都需要展示，因此做成一个component -->
             <common-aside></common-aside>
         </el-aside>
         <el-container>
             <el-header>Header</el-header>
             <el-main>
+                <!-- 主体区域以后会成为各个页面的展现区域，通过路由跳转实现 -->
                 <router-view></router-view>
             </el-main>
         </el-container>
