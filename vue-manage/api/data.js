28c9dc25a3db62b1ada55f564c271070145714f1
zhixiang-pan <zhixiang_pan@trendmicro.com>
2022-07-05 10:29:01 +0800

    封装axios

---
+++ vue-manage/api/data.js
@@ -0,0 +1,18 @@
+import axios from './axios'
+
+
+export const getMenu = (param) => {
+    return axios.request({
+        url: '/permission/getMenu',
+        method:'post',
+        data: param
+    })
+}
+
+export const getData = (param) => {
+    return axios.request({
+        url: '/home/getData',
+        method: 'get',
+        data: param
+    })
+}
