b7cbac260fcb3e8855f31a6dc1c45e141dce7ed2
zhixiang <1906390603@qq.com>
2022-09-08 21:19:39 +0800

    完善common header 功能

--- vue3-manage/src/components/CommonHeader.vue
+++ vue3-manage/src/components/CommonHeader.vue
@@ -8,6 +8,51 @@
             </el-button>
         </div>
         <div class="r-content">
+            <el-dropdown>
+                <span class="el-dropdown-link">
+                    <img class="user" :src="getImgSrc()" alt="头像" />
+                </span>
+                <template #dropdown>
+                    <el-dropdown-menu>
+                        <el-dropdown-item>我的</el-dropdown-item>
+                        <el-dropdown-item>退出</el-dropdown-item>
+                    </el-dropdown-menu>
+                </template>
+            </el-dropdown>
         </div>
     </el-header>
 </template>
+
+<script>
+export default {
+    setup() {
+        const getImgSrc = () => {
+            // 参考https://cn.vitejs.dev/guide/assets.html#new-url-url-import-meta-url
+            console.log(import.meta.url)
+            console.log(new URL("../assets/images/user.png", import.meta.url))
+            return new URL("../assets/images/user.png", import.meta.url).href;
+        };
+        return {
+            getImgSrc
+        }
+    }
+}
+
+</script>
+
+<style lang="less" scoped>
+header {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    width: 100%;
+}
+.r-content {
+    .user {
+        width: 40px;
+        height: 40px;
+        border-radius: 50%;
+    }
+}
+
+</style>
