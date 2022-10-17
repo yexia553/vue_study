6c00c6f0e2c571c99f06d056f89289769454abf0
zhixiang-pan <zhixiang_pan@trendmicro.com>
2022-10-17 17:51:53 +0800

    设置整体布局

--- vue3-notes/src/main.js
+++ vue3-notes/src/main.js
@@ -1,5 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
 import router from './router/index.js'
 
 import './assets/main.css'
@@ -7,4 +9,5 @@ import './assets/main.css'
 const app = createApp(App)
 
 app.use(router)
+app.use(ElementPlus)
 app.mount('#app')
---
+++ vue3-notes/src/views/Main.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="common-layout">
+        <el-container>
+            <el-aside width="200px">Aside</el-aside>
+            <el-container>
+                <el-header>Header</el-header>
+                <el-main>Main</el-main>
+            </el-container>
+        </el-container>
+    </div>
+</template>
+
+<style lang="less" scoped>
+.common-layout {
+    height: 100%;
+}
+</style>
